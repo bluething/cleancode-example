924d70725f0b3eb5030f06e4824c5342a3cd5eb3
bluething <habib.gunadarma@gmail.com>
2021-12-01 15:31:52 +0700

    Refactor nested if-else
    Check for simple conditions first and return immediately

--- src/main/java/io/github/bluething/cleancode/example/ch3/ReturnEarly.java
+++ src/main/java/io/github/bluething/cleancode/example/ch3/ReturnEarly.java
@@ -2,17 +2,14 @@ package io.github.bluething.cleancode.example.ch3;
 
 public class ReturnEarly {
     private boolean systemIsUp;
-    
+
     public String getPersonalInfo(Person person, int pin){
-        if (systemIsUp){
-            if (person != null && person.getName().equals("")){
-                if(person.getPin() != pin){
-                    return  "Invalid pin";
-                }
-                return "Invalid Name";
-            }
-            return "System is down at the moment";
-        }
+        if(!systemIsUp) return "System is down at the moment";
+
+        if(person != null && person.getName().equals("")) return "Invalid Name";
+
+        if(person.getPin() != pin) return "Invalid pin";
+
         return person.getPersonalInfo();
     }
 }
